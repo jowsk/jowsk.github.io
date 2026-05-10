2d74e5cdd0585861927a3058250585f3225a2d2d
jowsk <45643365+jowsk@users.noreply.github.com>
2026-05-10 16:13:23 -0400

    change icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,11 +20,11 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: '/icon-light-32x32.png',
+        url: '/robot_black.png',
         media: '(prefers-color-scheme: light)',
       },
       {
-        url: '/icon-dark-32x32.png',
+        url: '/robot_white.png',
         media: '(prefers-color-scheme: dark)',
       },
       {
@@ -32,7 +32,7 @@ export const metadata: Metadata = {
         type: 'image/svg+xml',
       },
     ],
-    apple: '/apple-icon.png',
+    apple: '/robot.png',
   },
 }
 
